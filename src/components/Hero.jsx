d040866f53526df5610e3488d535bf7f3f24d8af
Emily Kanarek <emilyakanarek@gmail.com>
2024-07-19 18:42:02 -0400

    Set up Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
 export default function Hero({ title, lead }) {
   return (
-    <section>
-      <h1>{title}</h1>
-      <p>{lead}</p>
-    </section>
+    <Container fluid className="text-center py-5 bg-light">
+      <Row className="py-lg-5">
+        <Col lg="6" md="8" className="mx-auto">
+          <h1 className="fw-light">{title}</h1>
+          <p className="lead text-muted">{lead}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
